1169bc815cd145c80a5b2efaab25197f056cb449
code-ft-pathum <pathum@example.com>
2026-03-04 16:01:42 +0530

    Move Firebase configs to .env and ensure environment variable usage

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBv8UxS6GxKcoWV0PCX3vJUfH3NrUMFTsk",
-    authDomain: "todaynews-bd81f.firebaseapp.com",
-    projectId: "todaynews-bd81f",
-    storageBucket: "todaynews-bd81f.firebasestorage.app",
-    messagingSenderId: "427717306853",
-    appId: "1:427717306853:web:b62624634a3aa7e118270c",
-    measurementId: "G-DYFRY43PVY"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
